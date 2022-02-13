64ffe1284064bd7a8ccda6c3df9bfbec01b8993d
Carl Marino <carlcmarino@protonmail.com>
2022-02-13 10:36:18 +0100

    uploaded code

---
+++ app.js
@@ -0,0 +1,22 @@
+const calcBtn = document.querySelector(".calc");
+const KH = document.querySelector(".kh");
+const per = document.querySelector(".per");
+const amount = document.querySelector(".amount");
+const keGrams = document.querySelector(".ke-grams");
+const mult = document.querySelector(".mult");
+const output = document.querySelector(".output");
+const allInputs = document.querySelectorAll("input");
+
+calcBtn.addEventListener("click", ()=>{
+    let = inputsEmpty = false; 
+    allInputs.forEach((input) =>{
+        if(input.value == ""){
+            output.innerHTML = "Bitte alle Felder ausfüllen"
+            inputsEmpty=true;
+        }
+    })
+    if(!inputsEmpty){
+        output.innerHTML = (((amount.value/per.value)*KH.value)/keGrams.value)*mult.value;
+    }
+    return;
+})
