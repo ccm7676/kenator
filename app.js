45a336e362f24b9cfac4978116a08335a8321d9a
Carl Marino <50554024+ccm7676@users.noreply.github.com>
2022-04-15 15:25:25 +0200

    added english support

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ const amount = document.querySelector(".amount");
 const mult = document.querySelector(".mult");
 const output = document.querySelector(".output");
 const allInputs = document.querySelectorAll("input");
+const perText = document.querySelector(".per-txt");
+
+
+
+const errMsg =  "Please fill out all fields"
+const lang = navigator.language || navigator.userLanguage;
+
+//set lang if german to german
+
+if(lang == "de"){
+    errMsg = "Bitte alle Felder ausfüllen";
+    KH.placeholder = "Kohlenhydrate";
+    perText.innerHTML = "pro";
+    per.placeholder = "Menge(z.B. 100g)";
+    amount.placeholder = "Menge an Essen";
+    mult.placeholder ="Multiplikator";
+    calcBtn.innerHTML = "Ausrechnen";
+
+}
 
 calcBtn.addEventListener("click", ()=>{
     let = inputsEmpty = false; 
     allInputs.forEach((input) =>{
         if(input.value == ""){
-            output.innerHTML = "Bitte alle Felder ausfüllen"
+            output.innerHTML = errMsg
             inputsEmpty=true;
         }
     })
